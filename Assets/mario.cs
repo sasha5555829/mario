33cfd231cbaffa03d9a0df6fa15ba7cd9f7cc232
sasha5555829 <sasamaslakov534@gmail.com>
2023-10-12 18:59:10 +0300

    run1

--- Assets/mario.cs
+++ Assets/mario.cs
@@ -2,34 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float speed = 10f;
     private Rigidbody2D rb;
     public float horizontal;
-    private bool flipRight = true;
-    void Start()
+    private bool flip = true;
+
+    void Player.Start()
     {
-        rb = GetComponent<Ridibody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    Update()
     {
-           horizontal = Input.GetAxis("Horizontal")
-           rb.velocity = new Vector2(horizontal, rb.velocity.y);
-           if (horizontal< 0 && !flipRight)
-           {
-                Flip();
-}
-           else if (horizontal < 0 && flipright) ; ;
-{
-    Flip();
-}
+        horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (horizontal > 0 && !flip)
+        {
+            void Player.Flip();
+        }
+        else if (horizontal < 0 && flip)
+        {
+            void Player.Flip();
+        }
     }
-    void Flip()
-{
-    flipRight = !flipRight;
-    Vector3 theScale = transform.localScale;
-    theScale.x = theScale.x * (-1);
-    transform.localScale = theScale;
-}
+
+
+    void Player.Flip()
+    {
+        flip = !flip;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
