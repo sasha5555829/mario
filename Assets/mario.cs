b3b7bafe136b51f632d37620ccde1f143fb67585
sasha5555829 <sasamaslakov534@gmail.com>
2023-10-15 10:03:29 +0300

    run

--- Assets/mario.cs
+++ Assets/mario.cs
@@ -1,41 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class mario : MonoBehaviour
 {
     public float speed = 10f;
     private Rigidbody2D rb;
     public float horizontal;
     private bool flip = true;
+    public Animator animator;
 
-    void Player.Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    Update()
+    void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-
-        if (horizontal > 0 && !flip)
+        animator.SetFloat("moveX",Math.Abs(horizontal));
+        if ((horizontal > 0 && !flip) || (horizontal < 0 && flip))
         {
-            void Player.Flip();
-        }
-        else if (horizontal < 0 && flip)
-        {
-            void Player.Flip();
+            transform.localScale *= new Vector2(-1, 1);
+            flip = !flip;
         }
     }
 
-
-    void Player.Flip()
-    {
-        flip = !flip;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
 }
